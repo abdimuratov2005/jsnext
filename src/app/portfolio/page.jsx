66831e33ca1837a15feaf243e755ca55b5ceed94
ffcoder-hub <program88@list.rui>
2023-09-04 15:00:41 +0300

    Portfolio : исправление конфликта React Spring (на будущее, его нельзя вызывать в callback или цикле, луче использовать useSprings)

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,11 +4,11 @@ import styles from "@/app/portfolio/portfolio.module.scss";
 import {typeLines} from "@/components/Pages/Portfolio/PortfolioMain/type_lines";
 import {motion, useAnimation} from "framer-motion";
 import PortfolioItems from "@/components/Pages/Portfolio/PortfolioItems";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {DataDevelopContext} from "@/app/contexts/DataDevelopContext";
 import {useContext} from "react";
 import PortfolioPopup from "@/components/Pages/Portfolio/PortfolioPopup";
-import { useSpring, animated, useChain } from '@react-spring/web';
+import {useSpring, animated, useChain, useSpringRef, useTransition, useSprings} from '@react-spring/web';
 
 export const metadata = {
     title: 'MapBiz - Portfolio',
@@ -19,77 +19,71 @@ export default function Portfolio() {
 
     const { isPopupActive, setPopup } = useContext(DataDevelopContext);
 
-    // const [ animateData, setAnimateData ] = useState(0)
-
-    // const handleButtonClick = () => {
-    //     // Увеличиваем значение на 1 при каждом нажатии кнопки
-    //     setAnimateData(animateData + 50);
-    // };
-
     function handlePopup () {
         console.log('true')
         setPopup(true)
     }
 
-    const [springs, api] = useSpring(() => ({
-        from: { x: 0 },
-    }))
+    // Анимация контейнера с текстовыми линиями
+    const propsTypes = useSpring({
+        from: { scale: 1, rotate: 0 },
+        to: [
+            { scale: 2.7, rotate: -90, },
+        ],
+        config: { duration: 700 },
+        delay: 500,
+    });
 
-    const handleClick = () => {
-        api.start({
-            from: {
-                x: 0,
-            },
-            to: {
-                x: 100,
-            },
-        })
+    // Подготовка delay значений для каждой текстовой линии
+    const props = [];
+    for(let i = 0; i < 15; i ++){
+        props[i] = 300 + 50 * i;
     }
+
+    const animatedProps = props.map((delayValue) => ({
+        from: { x: 0 },
+        to: { x: 20 },
+        config: { duration: 800 },
+        delay: delayValue,
+    }));
+
+    const animations = useSprings(
+        animatedProps.length,
+        animatedProps.map((animatedProp) => animatedProp)
+    );
+
+    const propsContent = useSpring({
+        from: { y: 0, opacity: 1 },
+        to: { y: 100, opacity: 0 },
+        config: { duration: 800 },
+        delay: 700,
+    });
 
     return (
         <>
-            <main className={`${styles.container} pt-[240px]`}>
+            <animated.div style={propsContent} className={`${styles.container} pt-[240px]`}>
 
-                <div
-                    className={`${styles.type} fixed h-screen w-screen lowercase flex flex-col justify-center items-center text-center font-grotesk text-whitetype-mapbiz mt-[20vh] `}
+                <animated.div
+                    className={`${styles.type} fixed h-screen w-screen lowercase flex flex-col justify-center items-center text-center font-grotesk text-whitetype-mapbiz mt-[20vh]`}
                     data-type-transition=""
                     aria-hidden="true"
+                    style={propsTypes}
                 >
-                    {
-                        typeLines && typeLines.ru.map((item, index) => {
-                            return <motion.div
-                            //     animate={{
-                            //     x: animateData, y: 0, scale: 1
-                            // }}
-                                initial={{
-                                    x: 0
-                                }}
-                                animate={{
-                                    x: 200
-                                }}
-                                key={ index }  className="type__line select-none">{ item }</motion.div>
-                        })
-                    }
+                    {animations.map((animation, index) => (
+                        <animated.div key={index} style={{ transform: animation.x.interpolate((x) => `translateX(${x}px)`) }}>
+                            {typeLines.ru[index]}
+                        </animated.div>
+                    ))}
 
-                </div>
-                <animated.div
-                    onClick={handleClick}
-                    style={{
-                        width: 80,
-                        height: 80,
-                        background: '#ff6d6d',
-                        borderRadius: 8,
-                        ...springs
-                    }}
-                />
-                {/*<button onClick={handleButtonClick}>Click</button>*/}
+                </animated.div>
+
                 <div className={styles.wrapper}>
                     <PortfolioItems />
                     <div className="flex justify-end mt-[190px] mb-1.5 text-white underline hover:no-underline">
                         <button onClick={handlePopup}>Весь список работ</button>
                     </div>
                 </div>
-            </main>
+            </animated.div>
             <NextPage url={'/contact'} image={`/img/portfolio/next-portfolio.webp`}></NextPage>
             <PortfolioPopup />
         </>
